73688647e0e04cdb65f72c3a7135627fb22775a9
Brad Anderson <brad.anderson87@gmail.com>
2016-06-12 15:23:19 -0500

    Fixes typo. Extra space.

--- tasks/static/watch.js
+++ tasks/static/watch.js
@@ -3,7 +3,7 @@ const env = require('../../build-env.js');
 
 
 module.exports = (gulp, done) => {
-    const watcher = gulp.watch(`${env.DIR_SRC}/**/*.html`, [ 'static:build ']);
+    const watcher = gulp.watch(`${env.DIR_SRC}/**/*.html`, [ 'static:build' ]);
 
     watcher.on('change', e => {
         gutil.log(`File ${e.path} was ${e.type}`);
